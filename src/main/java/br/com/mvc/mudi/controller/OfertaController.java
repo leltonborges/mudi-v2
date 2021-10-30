37ade6cfda2a2b5fb152dff7efe166656b1c619f
Lelton Borges <leltondev@gmail.com>
2021-10-30 18:59:54 -0300

    update menu

--- src/main/java/br/com/mvc/mudi/controller/OfertaController.java
+++ src/main/java/br/com/mvc/mudi/controller/OfertaController.java
@@ -1,15 +1,18 @@
 package br.com.mvc.mudi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/oferta")
+@RequestMapping(value = "/ofertas")
 public class OfertaController {
 
     @GetMapping
-    public String getFormOfertas(){
+    public String getFormOfertas(Model model){
+        String value = "ofertas";
+        model.addAttribute("activeMenu", value);
         return  "/oferta/home";
     }
 }
